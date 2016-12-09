d90120c812974261b9d473bee9d051b1adce9b3a
John French <hello@johnfrench.xyz>
2016-12-09 11:11:24 -0500

    Updates styles of side nav

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(document).foundation();
 
+// Mobile Nav Toggle Behavior
 $('#localNavToggle').on('click', function() {
     $('.localnav').slideToggle(200);
+});
+// Window Resize Check
+$(window).on('resize', function() {
+    if ( $(window).width() > 640 ) {
+        $('.localnav').css('display', 'block');
+    } else {
+        $('.localnav').css('display', 'none');
+    }
 });
