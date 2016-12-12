dfa7c19c110cde3d1230d630c33921050f46b0a9
John French <hello@johnfrench.xyz>
2016-12-12 14:51:58 -0500

    Updates mobile sid-nav behavior and adds arrow

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 $(document).foundation();
 
+// Mobile Navigation
+//------------------
+
 // Mobile Nav Toggle Behavior
 $('#localNavToggle').on('click', function() {
     $('.localnav').slideToggle(200);
+    if ($('#arrowToggle').attr('src') === 'images/down-arrow.png') {
+        $('#arrowToggle').attr('src', 'images/up-arrow.png');
+    } else {
+        $('#arrowToggle').attr('src', 'images/down-arrow.png');
+    }
 });
-// Window Resize Check
+// Mobile Nav Window Resize Check
 $(window).on('resize', function() {
+    // Hide the Nav on Small Displays
     if ( $(window).width() > 640 ) {
         $('.localnav').css('display', 'block');
     } else {
         $('.localnav').css('display', 'none');
     }
+    // Return the Arrow to the 'Up' State
+    $('#arrowToggle').attr('src', 'images/down-arrow.png');
 });
